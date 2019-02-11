f6f46d47caf24e2e07213f63177a216d0c01ac7d
Igor Nikitin <rabbitinspace@icloud.com>
2019-02-11 21:38:38 +1100

    Initial Commit

---
+++ Logoot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Logoot
+//
+//  Created by Igor Nikitin on 11/2/19.
+//  Copyright © 2019 Igor Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
